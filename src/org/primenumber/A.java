d1b6dd6dad8427761db9de98e2a10dfc057dbdd2
aravinthankumar <aravinthankumara@gmail.com>
2022-05-05 18:58:46 +0530

    master

---
+++ src/org/primenumber/A.java
@@ -0,0 +1,76 @@
+package org.primenumber;
+
+public class A {
+
+public static void main(String[] args) {
+	
+//	int a=100;
+//	int b=2;
+//	int c=3;
+//	int d=5;
+//	int count=0;
+//	System.out.println(b+""+c+""+d);
+//	for (int i = 2; i < a; i++) {
+//		
+//		
+//if(i%2!=0 && i%3!=0 && i%5!=0) {
+//	System.out.println(i);
+//	count++;
+//	
+//}
+//
+//
+//}
+//	System.out.println("hsgdsyhdy"+count);
+//	}	
+
+
+//	for (int i = 0; 5; i++) {
+//		System.out.println("hii");
+//		break;
+//		
+//	}
+	
+	char a='A';
+	a++;
+	System.out.println((int)a);
+	
+	
+	String  s1="hii";
+	String s2=new String("hii");
+	System.out.println(s1==s2);
+	
+//	final String a1;
+//	a1="hi";
+//	a1="hello";
+//	System.out.println(a1);
+//			
+	
+	int x=1;
+	System.out.println(x--);
+	
+//	int x1=1;
+//	System.out.println(--x1--);
+//	
+	
+	String s = new String("java");
+	s.concat("selenium").replace('s', 'S').toUpperCase().replace('S', 's').toString();
+	System.out.println(s);
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
+}
